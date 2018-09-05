37f8dc7319ec3f121793a810f1dcb73559938a1a
Alex Lindsay <alexlindsay239@gmail.com>
2018-09-05 15:09:09 -0600

    Initial mortar work. Things compile

---
+++ framework/include/constraints/MortarData.h
@@ -0,0 +1,24 @@
+#ifndef MORTARDATA_H
+#define MORTARDATA_H
+
+#include "AutomaticMortarGeneration.h"
+
+#include <map>
+
+class MortarData
+{
+public:
+  MortarData(SubProblem &);
+
+  AutomaticMortarGeneration & getMortarInterface(const std::pair<unsigned, unsigned> &);
+
+  void update();
+
+protected:
+  std::map<std::pair<unsigned, unsigned>, std::unique_ptr<AutomaticMortarGeneration>>
+      _mortar_interfaces;
+
+  SubProblem & _subproblem;
+};
+
+#endif
