8c95899a29680dd9acabd89893f6a622fc617787
Alex Lindsay <alexlindsay239@gmail.com>
2021-10-01 11:25:19 -0700

    Demo poor convergence in 1D NS test case

--- modules/navier_stokes/test/tests/finite_volume/ins/mms/channel-flow/cylindrical/cartesian-version/test.py
+++ modules/navier_stokes/test/tests/finite_volume/ins/mms/channel-flow/cylindrical/cartesian-version/test.py
@@ -4,6 +4,19 @@ import mms
 # import unittest
 from mooseutils import fuzzyEqual, fuzzyAbsoluteEqual
 
+# class Test1DRC(unittest.TestCase):
+class Test1DRC():
+    def test(self):
+        labels = ['L2u', 'L2p']
+        df1 = mms.run_spatial('1d-rc.i', 7, "--error", y_pp=labels, mpi=8)
+
+        fig = mms.ConvergencePlot(xlabel='Element Size ($h$)', ylabel='$L_2$ Error')
+        fig.plot(df1, label=labels, marker='o', markersize=8, num_fitted_points=3, slope_precision=1)
+        fig.save('1d-rc.png')
+        for key,value in fig.label_to_slope.items():
+            print("%s, %f" % (key, value))
+            # self.assertTrue(fuzzyAbsoluteEqual(value, 2., .1))
+
 # class Test2DRC(unittest.TestCase):
 class Test2DRC():
     def test(self):
@@ -19,4 +32,5 @@ class Test2DRC():
 
 if __name__ == '__main__':
     # unittest.main(__name__, verbosity=2)
-    Test2DRC().test()
+    # Test2DRC().test()
+    Test1DRC().test()
