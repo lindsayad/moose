33690c42cfb292781de9f6d2e11d399af523f58f
Alex Lindsay <alexlindsay239@gmail.com>
2021-10-06 11:46:32 -0700

    Second order run: remove momentum flux bcs
    So this should now be executing our momentum flux kernels on the
    boundary. This suggests to me that our momentum flux kernels are
    second order accurate when executing on boundaries at least when
    dirichlet information for the velocities (but not pressure in this
    case) are available

--- modules/navier_stokes/test/tests/finite_volume/ins/mms/channel-flow/cylindrical/cartesian-version/test.py
+++ modules/navier_stokes/test/tests/finite_volume/ins/mms/channel-flow/cylindrical/cartesian-version/test.py
@@ -34,7 +34,7 @@ class Test2DRC():
 class Test2DRCWalls():
     def test(self):
         labels = ['L2u', 'L2v', 'L2p']
-        df1 = mms.run_spatial('2d-rc-no-slip-walls.i', 5, "--error", y_pp=labels, mpi=8)
+        df1 = mms.run_spatial('2d-rc-no-slip-walls.i', 6, "--error", y_pp=labels, mpi=8)
 
         fig = mms.ConvergencePlot(xlabel='Element Size ($h$)', ylabel='$L_2$ Error')
         fig.plot(df1, label=labels, marker='o', markersize=8, num_fitted_points=3, slope_precision=1)
