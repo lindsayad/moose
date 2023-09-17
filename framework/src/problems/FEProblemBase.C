070bc805bf6b4420a116fe2cc083943b8229ce84
Alex Lindsay <alexander.lindsay@inl.gov>
2023-09-16 20:01:21 -0700

    When running displaced, make sure to prepare displaced data

--- framework/src/problems/FEProblemBase.C
+++ framework/src/problems/FEProblemBase.C
@@ -8106,13 +8106,6 @@ FEProblemBase::coordTransform()
   return mesh().coordTransform();
 }
 
-void
-FEProblemBase::reinitFVFace(const THREAD_ID tid, const FaceInfo & fi)
-{
-  for (auto & assembly : _assembly[tid])
-    assembly->reinitFVFace(fi);
-}
-
 unsigned int
 FEProblemBase::currentNlSysNum() const
 {
